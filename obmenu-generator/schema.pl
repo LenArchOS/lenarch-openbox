283d02017eea81f203d996f5737c2514735f475c
Lena Voytek <lena@ieee.org>
2021-06-15 20:25:13 -0700

    Added initial config file

---
+++ obmenu-generator/schema.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+# obmenu-generator - schema file
+# Modified By Lena Voytek
+
+require "$ENV{HOME}/.config/obmenu-generator/config.pl";
+
+our $SCHEMA = [
+	
+    {sep => 'All Apps'},
+    #          NAME            LABEL                ICON
+    {cat => ['utility',     'Accessories', 'applications-utilities']},
+    {cat => ['development', 'Development', 'applications-development']},
+    {cat => ['education',   'Education',   'applications-science']},
+    {cat => ['game',        'Games',       'applications-games']},
+    {cat => ['graphics',    'Graphics',    'applications-graphics']},
+    {cat => ['audiovideo',  'Multimedia',  'applications-multimedia']},
+    {cat => ['network',     'Network',     'applications-internet']},
+    {cat => ['office',      'Office',      'applications-office']},
+    {cat => ['other',       'Other',       'applications-other']},
+    {cat => ['settings',    'Settings',    'gnome-settings']},
+    {cat => ['system',      'System',      'applications-system']},
+
+    #{cat => ['consoleonly', 'CLI Applications',   'applications-utilities']},
+]
